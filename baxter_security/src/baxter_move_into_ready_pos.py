5cb997a9ed09f9c925fe967fa6984b8efa02dc44
Adrian Sudol <asudol@gmail.com>
2014-04-15 15:38:20 -0400

    Made changes to the wrist orientation movement from the hand cam

--- baxter_security/src/baxter_move_into_ready_pos.py
+++ baxter_security/src/baxter_move_into_ready_pos.py
@@ -19,17 +19,17 @@ from baxter_core_msgs.srv import (
     SolvePositionIKRequest,
 )
 
-class moveIntoready"
-    def __init__(self):
-      # Create this ros node
-      rospy.init_node("baxter_move_into_ready", anonymous=True)  
+class moveIntoReady:
+  def __init__(self):
+    # Create this ros node
+    rospy.init_node("baxter_move_into_ready", anonymous=True)  
 
-      # Subscribe to the kinect's coord publisher
-      rospy.Subscriber("/baxter_security/kinect_lighter_coords", Coords, self.coordsCallback)
+    # Subscribe to the kinect's coord publisher
+    rospy.Subscriber("/baxter_security/kinect_lighter_coords", Coords, self.coordsCallback)
 
-      rospy.spin()
+    rospy.spin()
 
-  def coordsCallback(self, data)
+  def coordsCallback(self, data):
     # Predetermined offsets from the kinect frame
     xoff = 0.899
     yoff = 0
@@ -47,10 +47,10 @@ class moveIntoready"
     zready = data.z - zoff
 
     # Move to the desired position, xy, then z
-    right = baxter_interface.Limb('right')
+    #right = baxter_interface.Limb('right')
     right.set_joint_position_speed(0.05)
     movement.moveTo(xready, yready, curZ)
     movement.moveTo(xready, yready, zready)
 
 if __name__ == '__main__':
-    pf = PosFind()
+    rdy = moveIntoReady()

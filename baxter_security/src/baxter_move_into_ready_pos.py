b13c36bfc5f125dbd95eb3e381d94d04f387cba8
Alec <alec@air.(none)>
2014-04-15 15:25:16 -0400

    Will move to xy then to z

--- baxter_security/src/baxter_move_into_ready_pos.py
+++ baxter_security/src/baxter_move_into_ready_pos.py
@@ -6,6 +6,18 @@ import baxter_interface
 import baxter_external_devices
 import movement
 from kinect_test.msg import Coords
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
 
 class moveIntoready"
     def __init__(self):
@@ -23,13 +35,21 @@ class moveIntoready"
     yoff = 0
     zoff = 0.7124
  
+    # Get the current position
+    right = baxter_interface.Limb('right')
+    pose = right.endpoint_pose()
+    curX = pose["position"].x
+    curY = pose["position"].y
+    curZ = pose["position"].z
+
     xready = xoff + data.x 
     yready = yoff + data.y
     zready = data.z - zoff
 
-    # Move to the desired position
+    # Move to the desired position, xy, then z
     right = baxter_interface.Limb('right')
     right.set_joint_position_speed(0.05)
+    movement.moveTo(xready, yready, curZ)
     movement.moveTo(xready, yready, zready)
 
 if __name__ == '__main__':

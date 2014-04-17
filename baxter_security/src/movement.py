395a9acec30a366ec8164f884e8e0d622ac1d30b
Corey Russell <cwrus@wpi.edu>
2014-04-17 14:27:05 -0400

    added a getClose function to move close to a point.

--- baxter_security/src/movement.py
+++ baxter_security/src/movement.py
@@ -21,6 +21,9 @@ from baxter_core_msgs.srv import (
     SolvePositionIKRequest,
 )
 
+global found = False
+global delta = .01
+
 # Move x,y,z meters relative to the current position
 def moveRel(x, y, z):
 	#get the current position and orientation of the arm
@@ -107,3 +110,40 @@ def setWrist(theta):
 	#move to this joint command, maintaining all thetas but changing the wrist. 
 	right.move_to_joint_positions(joint_command)
 	
+def getClose(x, y, z):
+	global found, delta
+	right = baxter_interface.Limb('right')
+	pose = right.endpoint_pose()
+	curX = pose["position"].x
+	curY = pose["position"].y
+	curZ = pose["position"].z
+
+	if (not closeTo(x,y,z) and (not found):
+	newX = curX + x
+	newY = curY + y
+	newZ = curZ + z
+	#condense this position into a point variable
+	loc = Point(newX, newY, newZ)
+	#solve for the new joint positions given our current orientation and new x,y,z position
+	limb_joints = ik_solve("right", loc, pose["orientation"]) 
+	#if there's a valid solution, move to it. 
+	if (limb_joints != -1):
+		found = True
+		right.move_to_joint_positions(limb_joints)
+	else:
+		getClose(x+delta,y,z)
+		getClose(x,y+delta,z)
+		getClose(x,y,z+delta)
+
+
+def closeTo(x, y, z):
+	err = .1
+	right = baxter_interface.Limb('right')
+	pose = right.endpoint_pose()
+	curX = pose["position"].x
+	curY = pose["position"].y
+	curZ = pose["position"].z
+	if (curX < x+err and curX > x-err and curY < y+err and curY > y-err and curZ < z+err and curZ > z-err):
+		return True
+	else:
+		return False

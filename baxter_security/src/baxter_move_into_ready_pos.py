8421f2234a13d93f6d99933a6b18ee97fa808b38
Alec <alec@air.(none)>
2014-04-19 16:39:41 -0400

    Adding all changes from 4/19

--- baxter_security/src/baxter_move_into_ready_pos.py
+++ baxter_security/src/baxter_move_into_ready_pos.py
@@ -32,24 +32,17 @@ class moveIntoReady:
 
   def coordsCallback(self, data):
     # Initilize the 4 pre-set starting areas
-    bottomRight = [0.716638893979, -0.323182332641]
-    topRight = [0.826942198613, -0.337988538941]
-    bottomLeft = [0.756998869647, -0.163337925954]
-    topLeft = [0.839202464355, -0.163838047031]
-    # Predetermined offsets from the kinect frame
-    xoff = 0.9
-    yoff = 0
-    zoff = 0.7124
+    bottomRight = [0.761010389869, -0.279305582713, 0.184718242161]
+    topRight = [0.854727558874, -0.24278074904, 0.199642837114]
+    bottomLeft = [0.760846322039, -0.133527073353, 0.1932059736]
+    topLeft = [0.833713943767, -0.108011570937, 0.203745233174]
     shortest = [100, 100]
-    upperLim = 0.354
-    middle = 0.232
     # Find the closest approach point
     rightSide = 0.4
     leftSide = 0.04
     topEdge = 0.05
     bottomEdge = 0.31
     # Closer to right side
-    print data.x, data.y
     if abs(data.x-rightSide) < abs(data.x-leftSide):
       if abs(data.y-topEdge) < abs(data.y-bottomEdge):
         shortest = topRight
@@ -61,15 +54,7 @@ class moveIntoReady:
         shortest = topLeft
       else:
         shortest = bottomLeft
-	
-    print shortest 
-    # Get the current position
-    right = baxter_interface.Limb('right')
-    pose = right.endpoint_pose()
-    curX = pose["position"].x
-    curY = pose["position"].y
-    curZ = pose["position"].z
-    movement.translateTo(shortest[0], shortest[1])
+    movement.moveTo(shortest[0], shortest[1], shortest[2])
     rospy.signal_shutdown("At approach posisition")
 
 if __name__ == '__main__':
--- baxter_security/src/baxter_rhand_orient.py
+++ baxter_security/src/baxter_rhand_orient.py
@@ -14,28 +14,37 @@ from geometry_msgs.msg import (
     Quaternion,
 )
 
-global wristDone, armDone, kinectZ
-wirstDone = False
-armDone = False
-
+global wristDone
+global armDone
+global graspDone
+global kinectZ
 
 def setup():
+	global wristDone
+        global armDone 
+	global graspDone
+        wristDone = False	
+        armDone = False
+	graspDone = False
+
 	rospy.init_node("baxter_security_rhand_orient", anonymous=True)
 
   	rospy.Subscriber("/kinect_lighter_coords", KinectFloatCoords, setZcallback)
 
 
-	while not rospy.is_shutdown():
+	while not rospy.is_shutdown() and not graspDone:
 		message = rospy.wait_for_message("/lighter_coords", BaxterRHandCamCoords)
 		coordCallBack(message)
+
 def setZcallback(data):
 	global kinectZ
 	kinectZ = data.z	
 
 def coordCallBack(data):
-        global wristDone, armDone 
+        global wristDone
+        global armDone
+	global graspDone
 
-	
 	heightScale = 0.00075
 	widthScale = 0.00075
 	tol = 20
@@ -56,31 +65,42 @@ def coordCallBack(data):
 
 	#if True:
 		#print right.endpoint_pose()
-	if (abs(widthError) > tol and abs(heightError) > tol):
-		movement.translateRel(heightError*heightScale, widthError*widthScale)
-	elif abs(widthError) > tol:
-		movement.translateRel(0, widthError*widthScale)
-	elif abs(heightError) > tol:
-		movement.translateRel(heightError*heightScale, 0)
-	else:
-		print "Arm is good"
-       		armDone = True 
+	if not armDone:
+		if (abs(widthError) > tol and abs(heightError) > tol):
+			movement.translateRel(heightError*heightScale, widthError*widthScale)
+		elif abs(widthError) > tol:
+			movement.translateRel(0, widthError*widthScale)
+		elif abs(heightError) > tol:
+			movement.translateRel(heightError*heightScale, 0)
+		else:
+			print "Arm is good"
+       			armDone = True 
+	
+	if not wristDone:
+		if (abs(theta) > wristTol):
+			#if (orientation + theta < -math.pi):
+				#theta = theta + math.pi
+			#elif (orientation + theta > math.pi):
+				#theta = theta - math.pi
+			print orientation, orientation + theta
+			movement.setWrist(orientation + theta)
+		else:
+			print "Wrist is good"
+        		wristDone = True
 
-	if (abs(theta) > wristTol):
-		movement.setWrist(orientation + theta)
-	else:
-		print "Wrist is good"
-        	wristDone = True
-
-        if wristDone == True and armDone == True:
+        if wristDone == True and armDone == True and not graspDone:
 		right = baxter_interface.Limb('right')
     		pose = right.endpoint_pose()
 		curZ = pose["position"].z
-		objHeightOffTable = 91.44 - kinectZ           
-		targetZ = objHeightOffTable - 20.2
-		print targetZ
-
-
+		objHeightOffTable = 0.9144 - kinectZ           
+		targetZ = objHeightOffTable - 0.202
+		targetZ = targetZ + 0.065 #Slight tolerance
+		print "Down"
+                movement.moveRel(0,0,targetZ-curZ)
+		print "Up"
+		baxter_interface.Gripper("right").close(True)
+		movement.moveToZ(0.15)
+		graspDone = True
 
 if __name__ == '__main__':
 	setup()
